81100cbb3d9690b907272b2e89e3a48b41aa8532
Bach Le <bach@numoney.store>
2026-02-11 15:56:54 +0700

    Turn into a standalone project

---
+++ bgame.cmake
@@ -0,0 +1,33 @@
+option(RELOADABLE "Is the program reloadable" ON)
+
+set(CMAKE_C_STANDARD 23)
+set(CMAKE_C_EXTENSIONS OFF)
+
+function (add_bgame_app NAME SOURCES)
+	if (RELOADABLE)
+		add_library(${NAME} SHARED ${SOURCES})
+		set_target_properties(${NAME} PROPERTIES C_VISIBILITY_PRESET hidden)
+		target_link_libraries(${NAME} PRIVATE bgame)
+		if (NOT MSVC)
+			target_link_options(${NAME} PRIVATE
+				-Wl,--exclude-libs,ALL
+				-Wl,--no-whole-archive
+				-Wl,--no-undefined
+			)
+		endif ()
+		set_target_properties(${NAME} PROPERTIES
+			OUTPUT_NAME "${NAME}"
+			PREFIX ""
+		)
+
+		add_executable(${NAME}-loader "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/bgame/src/loader_stub.c")
+		target_link_libraries(${NAME}-loader PRIVATE bgame-loader)
+		set_target_properties(${NAME}-loader PROPERTIES
+			OUTPUT_NAME "${NAME}"
+			PREFIX ""
+		)
+	else ()
+		add_executable(${NAME} ${SOURCES})
+		target_link_libraries(${NAME} PRIVATE bgame bgame-loader bgame-loader-stub)
+	endif ()
+endfunction ()
