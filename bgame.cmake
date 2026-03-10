669c58d1356b463a0eb6210233cc16f67606ec09
Bach Le <bach@bullno1.com>
2026-03-10 20:45:11 +0800

    Make adjustments for emscripten build

--- bgame.cmake
+++ bgame.cmake
@@ -30,15 +30,48 @@ function (add_bgame_app NAME SOURCES)
 		add_executable(${NAME} ${SOURCES})
 		target_link_libraries(${NAME} PRIVATE bgame bgame-loader bgame-loader-stub)
 	endif ()
+
+	if (EMSCRIPTEN)
+		set_target_properties(${NAME} PROPERTIES OUTPUT_NAME "${NAME}" SUFFIX ".html")
+		target_compile_options(${NAME} PRIVATE
+			-fno-rtti
+			-fno-exceptions
+			-gsplit-dwarf
+		)
+		target_link_options(${NAME} PRIVATE
+			-sASYNCIFY=1
+			-sALLOW_MEMORY_GROWTH=1
+			-gseparate-dwarf
+		)
+	endif ()
 endfunction ()
 
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR})
-set_target_properties(physfs PROPERTIES
-    C_STANDARD 99
-    C_STANDARD_REQUIRED ON
-    C_EXTENSIONS OFF
+
+if (RELOADABLE)
+	set(PHYSFS_TARGET physfs)
+else ()
+	set(PHYSFS_TARGET physfs-static)
+endif ()
+
+set_target_properties("${PHYSFS_TARGET}" PROPERTIES
+	C_STANDARD 99
+	C_STANDARD_REQUIRED ON
+	C_EXTENSIONS OFF
 )
-target_compile_definitions(physfs PRIVATE _POSIX_C_SOURCE=200112L)
-get_target_property(S2N_OPTS s2n COMPILE_OPTIONS)
-list(REMOVE_ITEM S2N_OPTS "-std=gnu99")
-set_target_properties(s2n PROPERTIES COMPILE_OPTIONS "${S2N_OPTS}")
+target_compile_definitions("${PHYSFS_TARGET}" PRIVATE _POSIX_C_SOURCE=200112L)
+
+if (EMSCRIPTEN)
+	target_compile_definitions("${PHYSFS_TARGET}" PRIVATE PHYSFS_PLATFORM_LINUX=1)
+
+	add_custom_target(copy-emscripten-shell
+		COMMAND ${CMAKE_COMMAND} -E copy_directory_if_different ${CMAKE_CURRENT_LIST_DIR}/emscripten ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/emscripten
+	)
+	add_library(bgame-emscripten-shell INTERFACE)
+	add_dependencies(bgame-emscripten-shell copy-emscripten-shell)
+	target_link_options(bgame-emscripten-shell INTERFACE --shell-file "${CMAKE_CURRENT_LIST_DIR}/emscripten/shell.html")
+else ()
+	get_target_property(S2N_OPTS s2n COMPILE_OPTIONS)
+	list(REMOVE_ITEM S2N_OPTS "-std=gnu99")
+	set_target_properties(s2n PROPERTIES COMPILE_OPTIONS "${S2N_OPTS}")
+endif ()
